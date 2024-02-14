b8f9eee348b4ab3dc256454d48e362717f7f7cee
Andrew Nicols <andrew@nicols.co.uk>
2024-02-14 13:03:52 +0800

    Unit tests and testcases classes should be final/abstract final/abstract
    Fixes #102

---
+++ moodle/Tests/Sniffs/PHPUnit/TestcaseAbstractSniffTest.php
@@ -0,0 +1,65 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace MoodleHQ\MoodleCS\moodle\Tests\Sniffs\PHPUnit;
+
+// phpcs:disable moodle.NamingConventions
+
+use MoodleHQ\MoodleCS\moodle\Tests\MoodleCSBaseTestCase;
+
+/**
+ * Test the TestCasesAbstractSniff sniff.
+ *
+ * @copyright  2024 Andrew Lyons <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * @covers \MoodleHQ\MoodleCS\moodle\Sniffs\PHPUnit\TestcaseAbstractSniff
+ */
+class TestcaseAbstractSniffTest extends MoodleCSBaseTestCase {
+
+    /**
+     * Data provider for self::provider_phpunit_data_returntypes
+     */
+    public static function phpunit_classes_final_provider(): array {
+        return [
+            'Standard fixes' => [
+                'fixture' => 'testcaseclassesabstract',
+                'errors' => [
+                ],
+                'warnings' => [
+                    8 => 'Testcase example_testcase should be declared as abstract.',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider phpunit_classes_final_provider
+     */
+    public function test_phpunit_classes_final(
+        string $fixture,
+        array $errors,
+        array $warnings
+    ): void {
+        $this->set_standard('moodle');
+        $this->set_sniff('moodle.PHPUnit.TestCasesAbstract');
+        $this->set_fixture(sprintf("%s/fixtures/%s.php", __DIR__, $fixture));
+        $this->set_warnings($warnings);
+        $this->set_errors($errors);
+
+        $this->verify_cs_results();
+    }
+}
